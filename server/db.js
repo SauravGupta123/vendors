5e4b15b6bc2d47a3b9515fcf77cc20a485ce366b
Anurag Sharma <71564387+anur4ag@users.noreply.github.com>
2024-03-06 21:03:32 +0530

    mongo added

--- server/db.js
+++ server/db.js
@@ -1,43 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const fetchData = async () => {
+  try {
+    const foodItemsFetch = mongoose.connection.db.collection("foodData");
+    const foodItemsData = await foodItemsFetch.find({}).toArray();
+
     try {
-      const foodItemsFetch =  mongoose.connection.db.collection('foodData');
-      const foodItemsData = await foodItemsFetch.find({}).toArray();
-
-      try{
-        const foodCatfetch =  mongoose.connection.db.collection('foodCategory');
-        const foodCatData = await foodCatfetch.find({}).toArray();
-        global.foodItems=foodItemsData;
-        global.foodCat=foodCatData;
-        
-
-      }
-      catch(err){
-          console.log("error fetching food category:", err);
-      }
-
-  
-
-      
-   
-  
-
+      const foodCatfetch = mongoose.connection.db.collection("foodCategory");
+      const foodCatData = await foodCatfetch.find({}).toArray();
+      global.foodItems = foodItemsData;
+      global.foodCat = foodCatData;
     } catch (err) {
-      console.log('Error fetching data:', err);
-      throw err; // Propagate the error if needed
+      console.log("error fetching food category:", err);
     }
-  };
-  
+  } catch (err) {
+    console.log("Error fetching data:", err);
+    throw err; // Propagate the error if needed
+  }
+};
+
 const connectMongoose = async () => {
   try {
-    const url = 'mongodb+srv://srv444:saurav123@gofooddb.ukrvosp.mongodb.net/goFoodDB?retryWrites=true&w=majority';
+    const url =
+      "mongodb+srv://anurag:F9QVUjEvw8PucYO1@vendors.6nmzpaa.mongodb.net/?retryWrites=true&w=majority&appName=vendors/vendorsDB";
     await mongoose.connect(url);
-    console.log('Connected to MongoDB');
-    const foodData= await fetchData();
-    // return foodData;
-  
+    console.log("Connected to MongoDB");
+    const foodData = await fetchData();
+    return foodData;
   } catch (err) {
-    console.log('Error connecting to MongoDB:', err);
+    console.log("Error connecting to MongoDB:", err);
   }
 };
 
